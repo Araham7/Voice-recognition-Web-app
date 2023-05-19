5a0125dbd9a5dd2ecd2cab7ee7c5ebb11b13c51d
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2023-05-19 17:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function record() {
   var recognition = new webkitSpeechRecognition();
-  recognition.lang = "en-GB";
-  // recognition.lang = "hi-GB";
+//   recognition.lang = "en-GB";
+  recognition.lang = "hi-GB";
   // recognition.lang = "pa-GB";
   // recognition.lang = "ur-GB";
   // recognition.lang = "bn-GB";
